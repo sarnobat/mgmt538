ac46295d8db452fae4a754bb14eb7ca54b4338c6
Sridhar Sarnobat <ss401533@gmail.com>
2013-06-30 18:31:10 -0700

    work in progress
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress
    
    work in progress

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ mkdir -p ~/.groovy/lib && cp .groovy/lib/*jar ~/.groovy/lib
 groovy server.groovy & 
 sleep 5
 open teacher.html 
-sleep 5
+sleep 5 # What until the the teacher's page is loaded before trying to load the student page
 open student.html
 fg
