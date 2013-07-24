28749b6c70a8c7c45b315f1f6f55cb1e25b82b51
Sridhar Sarnobat <ss401533@gmail.com>
2013-07-23 19:36:46 -0700

    Added TODO

--- server.groovy
+++ server.groovy
@@ -72,7 +72,7 @@ try {
 						//int updates = run.update( "UPDATE students SET raised=raised-1 WHERE name='" + name + "'");
 						statOperation = "UPDATE_RAISED_ROW";
 					}	
-					updateStats(run, teacherConnection, studentConnection, data, name, log, statOperation);
+					updateStats(run, teacherConnection, data, name, log, statOperation);
 				}
 			};
 		}
@@ -116,7 +116,7 @@ try {
 					if (data.startsWith("CORRECT::")) {
 						String name = data.substring(9,data.length());
 						try {
-							int updates = run.update( "UPDATE students SET correct=correct+1, raised=raised+2 WHERE name='" + name + "'");
+							int updates = run.update( "UPDATE students SET correct=correct+1, raised=raised+1 WHERE name='" + name + "'");
 							//int updates2 = run.update( "UPDATE students SET raised=raised+2 WHERE name='" + name + "'");
 						} catch (Exception e) {
 							log.info(e);
@@ -125,7 +125,8 @@ try {
 						// back the new stats for the student (from the database,
 						// don't try and be clever and increment it without querying the 
 						// database)
-						teacherConnection.sendMessage("UPDATE_STATS");
+					
+						updateStats(run, teacherConnection, data, name, log, "UPDATE_RAISED_ROW");	
 					}
 					// Send "CORRECT" toto the teacher for that student only
 					//
@@ -148,16 +149,17 @@ try {
 	e.printStackTrace();
 }
 
-void updateStats(QueryRunner run, Connection teacherConnection, Connection studentConnection, String data, String name, Logger log, String statOperation) {
+void updateStats(QueryRunner run, Connection teacherConnection, String data, String name, Logger log, String statOperation) {
 	Map[] studentRows = run.query("SELECT name,raised,correct FROM students WHERE name = ?", new MapListHandler(), name);
 	String raisedCount = studentRows.length > 0 ? studentRows[0].get("raised") : 0;
 	String correctCount = studentRows.length > 0 ? studentRows[0].get("correct") : 0;
 	try {
 		JSONObject json = new JSONObject();
-		json.put("name", data);
+		json.put("name", name);
 		json.put("raised", raisedCount);
 		json.put("correct", correctCount);
 		json.put("operation", statOperation);
+		log.info("updateStats() - about to send: " + json.toString());
 		teacherConnection.sendMessage(json.toString());
 	} catch (Exception x) {
 		log.info("json failure: " + x.toString());
